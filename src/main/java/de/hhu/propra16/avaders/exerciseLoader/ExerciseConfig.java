85450c5811bed83a543edd47345be92c87b2668a
Nikita Hasert <nikita.hasert@uni-duesseldorf.de>
2016-06-22 17:02:44 +0200

    Add ExerciseConfig.java
    ExerciseConfig holds 2 booleans, informing whether or not babysteps or tracking
    is required in the exercise this ExerciseConfig belongs to.

---
+++ src/main/java/de/hhu/propra16/avaders/exerciseLoader/ExerciseConfig.java
@@ -0,0 +1,11 @@
+package de.hhu.propra16.avaders.exerciseLoader;
+
+public class ExerciseConfig {
+	final boolean babySteps;
+	final boolean timeTracking;
+
+	public ExerciseConfig(boolean babySteps, boolean timeTracking){
+		this.babySteps = babySteps;
+		this.timeTracking = timeTracking;
+	}
+}
