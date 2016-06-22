257c105fcc4017c76dff383bad2ec73d5b497e4d
Nikita Hasert <nikita.hasert@uni-duesseldorf.de>
2016-06-22 16:44:36 +0200

    Add JavaFile and JavaFiles classes
    JavaFile holds the necessary information of a test or a classfile, like the sourceCodeTemplate
    and and className, while JavaFiles is a wrapper for lists of JavaFile instances.

---
+++ src/main/java/de/hhu/propra16/avaders/exerciseLoader/JavaFile.java
@@ -0,0 +1,11 @@
+package de.hhu.propra16.avaders.exerciseLoader;
+
+public class JavaFile {
+	public final String className;
+	public final String sourceCodeTemplate;
+
+	public JavaFile(String className, String sourceCodeTemplate){
+		this.className = className;
+		this.sourceCodeTemplate = sourceCodeTemplate;
+	}
+}
---
+++ src/main/java/de/hhu/propra16/avaders/exerciseLoader/JavaFiles.java
@@ -0,0 +1,20 @@
+package de.hhu.propra16.avaders.exerciseLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaFiles {
+	private List<JavaFile>	javaFiles;
+
+	public JavaFiles(){
+		javaFiles = new ArrayList<>();
+	}
+
+	void addJavaFile(JavaFile javaFile){
+		javaFiles.add(javaFile);
+	}
+
+	public JavaFile getJavaFile(int javaFileNumber){
+		return javaFiles.get(javaFileNumber);
+	}
+}
