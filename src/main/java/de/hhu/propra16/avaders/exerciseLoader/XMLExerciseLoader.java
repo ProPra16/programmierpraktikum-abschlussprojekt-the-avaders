ddbc1034ab3875be73c6e247a63e8509414aa6f9
Nikita Hasert <nikita.hasert@uni-duesseldorf.de>
2016-06-22 17:17:40 +0200

    Add ExerciseLoader.java

---
+++ src/main/java/de/hhu/propra16/avaders/exerciseLoader/XMLExerciseLoader.java
@@ -0,0 +1,10 @@
+package de.hhu.propra16.avaders.exerciseLoader;
+
+import java.nio.file.Path;
+
+public class XMLExerciseLoader implements ExerciseLoader {
+	@Override
+	public ExerciseCatalogue loadExercise(Path path) {
+		return null;
+	}
+}
