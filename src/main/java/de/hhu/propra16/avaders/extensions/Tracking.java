d496a3e8b18b75329b821b10247be4527f2e6afd
Lucas Döring <lucasdoering@Lucas-Macbook.local>
2016-06-24 10:31:10 +0200

    startet working on Tracking extension

---
+++ src/main/java/de/hhu/propra16/avaders/extensions/Tracking.java
@@ -0,0 +1,49 @@
+package de.hhu.propra16.avaders.extensions;
+
+import java.time.LocalTime;
+
+public class Tracking {
+	int secondsGREEN = 0, secondsRED = 0, secondsREFACTOR = 0;
+	LocalTime currentStartTime;
+	String currentState;
+
+	public void startedGREEN(){
+
+	}
+
+	public void finishedGREEN(){
+
+	}
+
+	public void startedRED(){
+
+	}
+
+	public void finishedRED(){
+
+	}
+
+	public void startedREFACTOR(){
+
+	}
+
+	public void finishedREFACTOR(){
+
+	}
+
+	public int getTimeForGREEN(){
+
+	}
+
+	public int getTimeForRED(){
+
+	}
+
+	public int getTimeForREFACTOR(){
+
+	}
+
+	public void showChart(){
+
+	}
+}
