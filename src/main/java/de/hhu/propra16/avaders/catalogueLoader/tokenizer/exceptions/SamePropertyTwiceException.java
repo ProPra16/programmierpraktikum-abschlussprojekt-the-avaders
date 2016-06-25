6d20cf0acf12996038ee55f48a505425919dd7ce
Nikita Hasert <nikitahasert@gmx.de>
2016-06-25 16:46:47 +0200

    Add SamePropertyTwiceException
    Thrown by StringParser to inform the user that the same property was found twice in the same line.

---
+++ src/main/java/de/hhu/propra16/avaders/catalogueLoader/tokenizer/exceptions/SamePropertyTwiceException.java
@@ -0,0 +1,8 @@
+package de.hhu.propra16.avaders.catalogueLoader.tokenizer.exceptions;
+
+public class SamePropertyTwiceException extends Exception {
+	public SamePropertyTwiceException(String property, int lineNr){
+		super("Line " + lineNr + " holds the property " + property + " twice,\n" +
+				"every property should only appear once.");
+	}
+}
