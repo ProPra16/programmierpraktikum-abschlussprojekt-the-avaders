44d5b5919a7877ae4abeb09a56b45ca5b67299b4
Batman140 <thiemo.urselmann@uni-duesseldorf.de>
2016-06-25 12:26:16 +0200

    changed base from grid- to borderpane

--- src/main/java/de/hhu/propra16/avaders/Main.java
+++ src/main/java/de/hhu/propra16/avaders/Main.java
@@ -2,20 +2,19 @@ package de.hhu.propra16.avaders;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception{
-		GridPane root = FXMLLoader.load(getClass().getResource("applicationView.fxml"));
+		BorderPane root = FXMLLoader.load(getClass().getResource("applicationView.fxml"));
 		primaryStage.setTitle("Test Driven Developement Trainer");
 		
 		
-		Scene scene = new Scene(root, 300, 275);
+		Scene scene = new Scene(root, 600, 400);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
