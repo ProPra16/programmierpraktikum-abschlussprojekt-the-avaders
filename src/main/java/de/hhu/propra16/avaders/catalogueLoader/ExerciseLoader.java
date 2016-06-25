2368c30449e0d04672e777f014bda0832d3cc7e0
Nikita Hasert <nikitahasert@gmx.de>
2016-06-25 17:30:58 +0200

    Add interface ExerciseLoader now incorparates SamePropertyTwiceException

--- src/main/java/de/hhu/propra16/avaders/catalogueLoader/ExerciseLoader.java
+++ src/main/java/de/hhu/propra16/avaders/catalogueLoader/ExerciseLoader.java
@@ -1,7 +1,10 @@
 package de.hhu.propra16.avaders.catalogueLoader;
 
+import de.hhu.propra16.avaders.catalogueLoader.tokenizer.exceptions.SamePropertyTwiceException;
+
+import java.io.IOException;
 import java.nio.file.Path;
 
 public interface ExerciseLoader {
-	ExerciseCatalogue loadExercise(Path path);
+	ExerciseCatalogue loadExercise(Path path) throws SamePropertyTwiceException, IOException;
 }
