891f7aa6f15b808dd96519a02d708d9ceccb4983
Batman140 <thiemo.urselmann@uni-duesseldorf.de>
2016-06-25 11:37:41 +0200

    made Main load application.css and applicationView.fxml

--- src/main/java/de/hhu/propra16/avaders/Main.java
+++ src/main/java/de/hhu/propra16/avaders/Main.java
@@ -1,6 +1,8 @@
 package de.hhu.propra16.avaders;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -9,9 +11,13 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception{
-		GridPane root = new GridPane();
-		primaryStage.setTitle("Hello World");
-		primaryStage.setScene(new Scene(root, 300, 275));
+		GridPane root = FXMLLoader.load(getClass().getResource("applicationView.fxml"));
+		primaryStage.setTitle("Test Driven Developement Trainer");
+		
+		
+		Scene scene = new Scene(root, 300, 275);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
