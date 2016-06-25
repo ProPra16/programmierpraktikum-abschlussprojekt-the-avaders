5aaa796095227dfb47a7336678831547b464fb6f
Nikita Hasert <nikitahasert@gmx.de>
2016-06-25 16:02:34 +0200

    Add Tester for WhiteSpaceRemover

---
+++ src/main/java/de/hhu/propra16/avaders/catalogueLoader/tokenizer/WhiteSpaceRemoverTester.java
@@ -0,0 +1,21 @@
+package de.hhu.propra16.avaders.catalogueLoader.tokenizer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WhiteSpaceRemoverTester {
+	@Test
+	public void test_Space(){
+		Assert.assertEquals("test", WhiteSpaceRemover.removeWhiteSpace("      test   "));
+	}
+
+	@Test
+	public void test_Tab(){
+		Assert.assertEquals("test", WhiteSpaceRemover.removeWhiteSpace("\t\t  \t test \t   "));
+	}
+
+	@Test
+	public void test_LineBreak(){
+		Assert.assertEquals("test", WhiteSpaceRemover.removeWhiteSpace("\t\t  \t test \t   \n"));
+	}
+}
