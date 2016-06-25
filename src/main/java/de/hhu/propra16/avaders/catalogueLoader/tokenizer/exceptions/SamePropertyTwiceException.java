08b465eb28d0b2a929b3d633963a1a4645409a0c
Nikita Hasert <nikitahasert@gmx.de>
2016-06-25 17:01:47 +0200

    Change Message to one liner
    The Message now only occupies one line, so that it is nicer to read.

--- src/main/java/de/hhu/propra16/avaders/catalogueLoader/tokenizer/exceptions/SamePropertyTwiceException.java
+++ src/main/java/de/hhu/propra16/avaders/catalogueLoader/tokenizer/exceptions/SamePropertyTwiceException.java
@@ -2,7 +2,6 @@ package de.hhu.propra16.avaders.catalogueLoader.tokenizer.exceptions;
 
 public class SamePropertyTwiceException extends Exception {
 	public SamePropertyTwiceException(String property, int lineNr){
-		super("Line " + lineNr + " holds the property " + property + " twice,\n" +
-				"every property should only appear once.");
+		super("Line " + lineNr + " holds the property " + property + " twice, every property should only appear once.");
 	}
 }
