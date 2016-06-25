f501b9d678e867d741f78e5015ab23f83b826316
dgelessus <dgelessus@users.noreply.github.com>
2016-06-25 16:09:51 +0200

    Make Logik.schritt an enum for clarity

--- src/main/java/de/hhu/propra16/avaders/logik/Logik.java
+++ src/main/java/de/hhu/propra16/avaders/logik/Logik.java
@@ -1,18 +1,29 @@
 package de.hhu.propra16.avaders.logik;
 
+import static de.hhu.propra16.avaders.logik.Logik.Step.*;
+
 public class Logik {
+	public static enum Step {
+		RED,
+		GREEN,
+		REFACTOR1,
+		REFACTOR2,
+		END,
+		DONE
+	}
+	
 	private ILogikKonfig fKonfig;
 	private ITester fTester;
-	private int schritt;
+	private Step schritt;
 	private boolean atdd;
 	private boolean refactor2;
 
       //Konstruktor: Erstellt eine Instanz von Logik
 	public Logik(ITester tester, ILogikKonfig konfig) {
-			fKonfig = konfig;                                       //Uebergabewerte speichern
-			fTester = tester;                                       //und Variablen initialisieren
-			schritt = 0;
-			konfigLaden();
+		fKonfig = konfig;                                       //Uebergabewerte speichern
+		fTester = tester;                                       //und Variablen initialisieren
+		schritt = RED;
+		konfigLaden();
 	}
 
 	//Lädt die relevanten Einstellungen aus der Konfiguration
@@ -26,11 +37,11 @@ public class Logik {
       //weiter: wird von GUI aufgerufen, testet und geht gegebenenfalls einen Zyklenschritt weiter
 	public ITestenRueckgabe weiter(ITestenUebergabe uebergabe) {
 		switch(schritt) {                                             //Aktuellen Schritt ermitteln
-			case 0: return redWeiter(uebergabe);
-			case 1: return greenWeiter(uebergabe);
-			case 2: return refactor1Weiter(uebergabe);
-			case 3: return refactor2Weiter(uebergabe);
-			case 4: endeWeiter();
+			case RED: return redWeiter(uebergabe);
+			case GREEN: return greenWeiter(uebergabe);
+			case REFACTOR1: return refactor1Weiter(uebergabe);
+			case REFACTOR2: return refactor2Weiter(uebergabe);
+			case END: endeWeiter();
 		}
 		return null;
 	}
@@ -38,14 +49,14 @@ public class Logik {
       //Versucht im Schritt RED, weiterzugehen
 	ITestenRueckgabe redWeiter(ITestenUebergabe uebergabe) {
 		ITestenRueckgabe rueckgabe = fTester.Testen(uebergabe);       //Test ausführen
-		if (rueckgabe.testBestanden()) schritt = 1;                   //Red -> Green
+		if (rueckgabe.testBestanden()) schritt = GREEN;
 		return rueckgabe;                                             //Testergebnisse zurückgeben
 	}
 
       //Versucht im Schritt GREEN, weiterzugehen
 	ITestenRueckgabe greenWeiter(ITestenUebergabe uebergabe) {
 		ITestenRueckgabe rueckgabe = fTester.Testen(uebergabe);       //Test ausführen
-		if (rueckgabe.testBestanden()) schritt = 2;                   //Green -> Refactor 1
+		if (rueckgabe.testBestanden()) schritt = REFACTOR1;                   //Green -> Refactor 1
 		return rueckgabe;                                             //Testergebnisse zurückgeben
 	}
 
@@ -59,7 +70,7 @@ public class Logik {
       //Geht im Schritt REFACTOR1 weiter zu REFACTOR 2 oder zum Ende
 	void Refactor2Aufrufen(ITestenRueckgabe rueckgabe, ITestenUebergabe uebergabe) {
 		if (refactor2) {                                              //Zusatz: Refactor 2
-			if (rueckgabe.testBestanden()) schritt = 3;             //Refactor 1 -> Refactor 2
+			if (rueckgabe.testBestanden()) schritt = REFACTOR2;             //Refactor 1 -> Refactor 2
 		} else {
 			if (rueckgabe.testBestanden()) endeAufrufen(uebergabe); //Refactor 1 -> Ende
 		}
@@ -77,30 +88,30 @@ public class Logik {
 		if (atdd) {                                                    //Zusatz: Akzeptanztest ausführen
 			ITestenRueckgabe rueckgabe = fTester.Testen(uebergabe);  //Testen
 			if (rueckgabe.testBestanden()) {                         //Falls der Test bestanden wurde
-				if (fKonfig.naechsterTest()) schritt = 0;          //Ende -> Red
-				else schritt = 5;                                  //Entwicklungsende
+				if (fKonfig.naechsterTest()) schritt = RED;          //Ende -> Red
+				else schritt = DONE;                                  //Entwicklungsende
 			} else {
 				return rueckgabe;                                  //Testergebnisse zurückgeben
 			}
 		} else {
-			schritt = 0;                                             //Ende -> Red
+			schritt = RED;                                             //Ende -> Red
 		}
 		return null;                                                   //Kein Test ausgeführt, nichts zurückgeben
 	}
 
       //Startet den Zyklus mit einem neuen Test
 	void endeWeiter() {
-		schritt = 0;                                                   //Ende -> Red
+		schritt = RED;                                                   //Ende -> Red
 	}
 
       //Bricht den aktuellen Test ab und setzt das Programm zum letzten bestandendenen Test zurück
 	public void abbrechen() {
-		schritt = 0;
+		schritt = RED;
 	}
 
       //Bricht den aktuellen Test ab und setzt das Programm zum letzten bestandendenen Akzeptanztest zurück
 	public void zuruecksetzen() {
-		schritt = 0;
+		schritt = RED;
 		fKonfig.ErstenTestAuswaehlen();
 	}
 }
