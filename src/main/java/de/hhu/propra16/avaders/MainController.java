b58ad275883137f1289fc62a5f4f8dd7d64a9b4d
Batman140 <thiemo.urselmann@uni-duesseldorf.de>
2016-06-29 15:34:10 +0200

    added handler

--- src/main/java/de/hhu/propra16/avaders/MainController.java
+++ src/main/java/de/hhu/propra16/avaders/MainController.java
@@ -1,4 +1,45 @@
 package de.hhu.propra16.avaders;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
+public class MainController {
+
+    @FXML
+    private MenuItem newExercise;
+    @FXML
+    private Button prePhase;
+    @FXML
+    private Text activatedModes;
+    @FXML
+    private MenuItem restart;
+    @FXML
+    private Button nextPhase;
+    @FXML
+    private TextArea greenRefactor;
+    @FXML
+    private Text timeLeft;
+
+    
+    //Handler
+    @FXML
+    void handlePrePhase(ActionEvent event) {
+
+    }
+    @FXML
+    void handleNextPhase(ActionEvent event) {
+
+    }
+    @FXML
+    void handleNewExcercise(ActionEvent event) {
+
+    }
+    @FXML
+    void handleRestart(ActionEvent event) {
+
+    }
 }
