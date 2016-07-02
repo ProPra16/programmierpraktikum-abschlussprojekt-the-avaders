8f87c265ed5e2dc65874997079af8f9d4ddac603
Batman140 <thiemo.urselmann@uni-duesseldorf.de>
2016-07-02 19:08:19 +0200

    Built  interfaces and refactored structure in gui package

--- src/main/java/de/hhu/propra16/avaders/MainController.java
+++ src/main/java/de/hhu/propra16/avaders/MainController.java
@@ -1,10 +1,6 @@
 package de.hhu.propra16.avaders;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-
+import de.hhu.propra16.avaders.gui.ExerciseFilePath;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -12,30 +8,14 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
-import javafx.stage.FileChooser;
+
+import java.nio.file.Path;
 
 public class MainController {
-	
-	//access to main pane of primarystage where scene is set
+
 	private static BorderPane mainBase;
-	
-	//access to every phase-object
-	private static Map<PhaseName,Phase> phases;
-	
-	/**
-	 * main cycle-control datastructure:
-	 * Objects Red,Green and Refactor will be inserted in this order. The first 
-	 * Element represents the current Phase. We do EVER access first element to 
-	 * set phase.
-	 * Switching can be achieved due shifting the list with the shiftoperations below.
-	 * For example:
-	 * list(Red,Green,Refactor) -> Red-phase --->shiftLeft---> list(Green,Refactor,Red) -> 
-	 * Green-Phase.
-	 */
-	private static LinkedList<Phase> phase = new LinkedList<Phase>();
-	private static File exercise;
-	
-	
+	private        Path       exerciseFilePath;
+
 	//excercise-menuItems
     @FXML private MenuItem newExercise;
     @FXML private MenuItem restart;
@@ -48,81 +28,23 @@ public class MainController {
     @FXML private Text activatedModes;
     @FXML private Text timeLeft;
 
-    
+    //input-areas for user
     @FXML private TextArea greenRefactor;
     @FXML private TextArea userFieldRed;
     @FXML private TextArea userFieldCode;
-    
-   
+
     //Handler
-    @FXML void handleRestart(ActionEvent event) { }
-    
-   
-    @FXML void handlePrePhase(ActionEvent event) {
-    	shiftRight(phase);
-    	phase.getFirst().setStates(userFieldRed, userFieldCode, stepBack, stepFurther);
-    	if(stepFurther.getText().contentEquals("Green")){
-     		userFieldCode.clear();
-    	}
-    }
-    
-    @FXML void handleNextPhase(ActionEvent event){
-    	shiftLeft(phase);
-    	//after clicking finish, the codefield will be cleared, current userinput will be overwritten at this
-    	//time when codeTextfield is called the very next time
-    	if(stepFurther.getText().contentEquals("Finish")){
-     		userFieldCode.clear();
-    	}
-    	phase.getFirst().setStates(userFieldRed, userFieldCode, stepBack, stepFurther);
+    @FXML void handleRestart(ActionEvent event)  {}
+    @FXML void handlePrePhase(ActionEvent event) {}
+    @FXML void handleNextPhase(ActionEvent event){}
+
+
+    @FXML void handleNewExercise(ActionEvent event) {
+        this.exerciseFilePath = new ExerciseFilePath().getExercise();
+        userFieldRed.setEditable(false);
+        userFieldCode.setDisable(false);
     }
 
-	/**
-	 * opens onClick on new... a window where an exercise  (xml-file)
-	 * should be choosen and only if one does the program continues.
-	 * Exercise will be saved to instance-var 'exercis' of type File,
-	 * can be converted to Path for xml-catalogue-loader
-	 */
-	public void chooseFile(){
-		FileChooser window = new FileChooser();
-		window.setTitle("Choose exercise");
-		window.getExtensionFilters().add( new FileChooser.ExtensionFilter("xml files", "*.xml"));
-		exercise = window.showOpenDialog(Main.getPrimaryStage());
-	}
 
-    /**
-     * will be triggered if click on excercise->new... in the menubar.
-     * takes the mainbase makes controls visible to user, creates map for
-     * general access to phase objects and initializes phase-control-list.
-     * finally sets first phase ->Red-phase.
-     * @param event: selecting new... -menuItem for choosing and starting exercise
-     */
-    @FXML void handleNewExcercise(ActionEvent event) {
-		chooseFile();
-		if(exercise == null) return;
-    	mainBase = Main.getMainBase();
-    	mainBase.getCenter().setVisible(true);
-    	mainBase.getBottom().setVisible(true);
-     	phases   = new HashMap<>();
-     	
-    	phases.put(PhaseName.RED, new Red());
-    	phases.put(PhaseName.GREEN, new Green());
-    	phases.put(PhaseName.REFACTOR, new Refactor());
-  
-    	phase.add(phases.get(PhaseName.RED));
-    	phase.addLast(phases.get(PhaseName.GREEN));
-    	phase.addLast(phases.get(PhaseName.REFACTOR));
-    	
-    	phase.getFirst().setStates(userFieldRed, userFieldCode, stepBack, stepFurther);
-    }
-    
- 
-    //Shift-operations mentioned above 
-    private void shiftLeft(LinkedList<Phase> phases){
-    	phase.addLast(phase.getFirst());
-    	phase.removeFirst();
-    }
-    private void shiftRight(LinkedList<Phase> phases){
-    	phase.addFirst(phase.getLast());
-    	phase.removeLast();
-    }
+
 }
