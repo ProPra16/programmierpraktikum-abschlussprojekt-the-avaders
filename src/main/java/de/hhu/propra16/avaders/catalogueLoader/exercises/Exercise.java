d4e9023782823531f83c1c2fe4f4b152bacad1f7
Nikita Hasert <nikitahasert@gmx.de>
2016-07-03 14:08:53 +0200

    Add comments

--- src/main/java/de/hhu/propra16/avaders/catalogueLoader/exercises/Exercise.java
+++ src/main/java/de/hhu/propra16/avaders/catalogueLoader/exercises/Exercise.java
@@ -18,50 +18,93 @@ public class Exercise {
 		this.exerciseConfig = exerciseConfig;
 	}
 
+	/**
+	 * @return The name of the exercise
+     */
 	public String getExerciseName() {
 		return exerciseName;
 	}
 
+	/**
+	 * @return The description of the exercise
+     */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * @param templateNr the index of the class of which the template
+	 *                   should be returned
+	 * @return The template of the class specified by templateNr
+     */
 	public String getClassTemplate(int templateNr){
 		return classTemplates.getJavaFile(templateNr).sourceCodeTemplate;
 	}
 
+	/**
+	 *
+	 * @param classNr the index of the test of which the template
+	 *                   should be returned
+	 * @return The template of the class specified by templateNr
+     */
 	public String getTestTemplates(int classNr){
 		return testTemplates.getJavaFile(classNr).sourceCodeTemplate;
 	}
 
+	/**
+	 * @return True if babysteps is enabled in this exercise
+     */
 	public boolean babyStepsIsEnabled(){
 		return exerciseConfig.isBabySteps();
 	}
 
+	/**
+	 * @return True if time tracking is enabled in this exercise
+     */
 	public boolean timeTrackingIsEnabled(){
 		return exerciseConfig.isTimeTracking();
 	}
 
+	/**
+	 * @return The time (as string), specified by babysteps, until all changes are reverted
+	 */
 	public String babyStepsTime(){
 		return exerciseConfig.getBabyStepsTime();
 	}
 
+	/**
+	 * @param classNr The index of the class of which the name should be returned
+	 * @return The name of the class
+     */
 	public String getClassName(int classNr){
 		return classTemplates.getJavaFile(classNr).className;
 	}
 
+	/**
+	 * @param testNr The index of the test of which the name should be returned
+	 * @return The name of the test
+     */
 	public String getTestName(int testNr) {
 		return testTemplates.getJavaFile(testNr).className;
 	}
 
+	/**
+	 * @return the number of classes in this exercise
+     */
 	public int getNrOfClasses(){
 		return classTemplates.size();
 	}
 
+	/**
+	 * @return the number of tests in this exercise
+     */
 	public int getNrOfTests(){
 		return testTemplates.size();
 	}
 
+	/**
+	 * @return True if atdd is enabled
+     */
 	public boolean atdd() {
 		return exerciseConfig.isAtdd();
 	}
