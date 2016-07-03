6de83a53a813d6b664d8b5664be5da6c81f898e7
Nikita Hasert <nikitahasert@gmx.de>
2016-07-03 15:49:23 +0200

    Add comments

--- src/main/java/de/hhu/propra16/avaders/catalogueLoader/tokenizer/exceptions/TokenException.java
+++ src/main/java/de/hhu/propra16/avaders/catalogueLoader/tokenizer/exceptions/TokenException.java
@@ -1,5 +1,8 @@
 package de.hhu.propra16.avaders.catalogueLoader.tokenizer.exceptions;
 
+/**
+ * Signals that an exception happened, while reading lexemes in the file
+ */
 public class TokenException extends Exception{
 	public TokenException(String msg){
 		super(msg);
