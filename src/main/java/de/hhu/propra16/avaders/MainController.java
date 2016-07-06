9d162724352247c6b025645574a98dbe4e46e318
Batman140 <thiemo.urselmann@uni-duesseldorf.de>
2016-07-06 20:52:03 +0200

    Controller: working on incorporating XMLExerciseLoader

--- src/main/java/de/hhu/propra16/avaders/MainController.java
+++ src/main/java/de/hhu/propra16/avaders/MainController.java
@@ -1,20 +1,30 @@
 package de.hhu.propra16.avaders;
 
+import de.hhu.propra16.avaders.catalogueLoader.ParserException;
+import de.hhu.propra16.avaders.catalogueLoader.XMLExerciseLoader;
+import de.hhu.propra16.avaders.catalogueLoader.exercises.ExerciseCatalogue;
+import de.hhu.propra16.avaders.catalogueLoader.tokenizer.FileReader;
+import de.hhu.propra16.avaders.catalogueLoader.tokenizer.XMLExerciseTokenizer;
+import de.hhu.propra16.avaders.catalogueLoader.tokenizer.exceptions.SamePropertyTwiceException;
+import de.hhu.propra16.avaders.catalogueLoader.tokenizer.exceptions.TokenException;
 import de.hhu.propra16.avaders.konfig.KonfigWerte;
 import de.hhu.propra16.avaders.logik.Logik;
 import de.hhu.propra16.avaders.testen.Tester;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 public class MainController {
-	private Main main;
-	private Path exerciseFilePath;
+	private Main       main;
+	private FileReader fileReader;
+	private Logik      logic;
 
 	//exercise-menuItems
     @FXML private MenuItem newExercise;
@@ -39,14 +49,21 @@ public class MainController {
     @FXML void handleNextPhase(ActionEvent event){}
 
     @FXML void handleNewExercise(ActionEvent event) {
-        this.exerciseFilePath = main.getExercise();
-        userFieldRed.setEditable(false);
-        userFieldCode.setDisable(false);
-    }
+		Path exercisePath = main.getExercise();
+		try {
+			FileReader fileReader = new FileReader(exercisePath);
+			XMLExerciseTokenizer xmlExerciseTokenizer = new XMLExerciseTokenizer(fileReader); //able to read tokens out of file
+			ExerciseCatalogue    exerciseCatalogue    = new ExerciseCatalogue(); //empty catalogue
+			XMLExerciseLoader    xmlExerciseLoader    = new XMLExerciseLoader(xmlExerciseTokenizer, exerciseCatalogue);
+		} catch (IOException | ParserException | TokenException | SamePropertyTwiceException e) {
+			e.printStackTrace();
+			if(e instanceof ParserException)
+				System.out.println("Caught null from main.getExercise: No file selected");
+		}
+	}
 
 	@FXML
 	public void initialize(){
-		//build up logic
 		Tester      tester      = new Tester();
 		KonfigWerte konfigWerte = new KonfigWerte();
 		Logik       logic       = new Logik(tester, konfigWerte);
