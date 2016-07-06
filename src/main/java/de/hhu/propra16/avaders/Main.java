8ad6b4abfaebc633a0d827a1de48978b8c309d63
Batman140 <thiemo.urselmann@uni-duesseldorf.de>
2016-07-06 14:50:29 +0200

    Refactoring main: refactored to initial main. same to maincontroller

--- src/main/java/de/hhu/propra16/avaders/Main.java
+++ src/main/java/de/hhu/propra16/avaders/Main.java
@@ -1,34 +1,63 @@
 package de.hhu.propra16.avaders;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main extends Application {
-	private static Stage      primaryStage;
-	private static BorderPane mainBase;
-	
-	public static BorderPane getMainBase() { return mainBase;}
+	private Stage          primaryStage;
+	private MainController controller;
+	private BorderPane     mainBase;
 
 	@Override
-	public void start(Stage stage) throws Exception{
-		primaryStage = stage;
-		mainBase = FXMLLoader.load(getClass().getResource("applicationView.fxml"));
-		primaryStage.setTitle("Test Driven Developement Trainer");
-		
+	public void start(Stage primaryStage) throws Exception{
+		initStage(primaryStage);
+		makeThisAccessibleFor(controller);
+	}
+
+	private void makeThisAccessibleFor(MainController controller){
+		this.controller.setMain(this);
+	}
+
+	//Initializes Stage, load and store MainController
+	private void initStage(Stage primaryStage){
+		this.primaryStage = primaryStage;
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("applicationView.fxml"));
+			this.mainBase     = loader.load();
+			this.controller   = loader.getController();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		primaryStage.setTitle("Test Driven Development Trainer");
 		Scene scene = new Scene(mainBase, 600, 400);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
-	public static void main(String[] args) {
-		launch(args);
+	public Path getExercise() {
+		FileChooser exerciseChoosePopup = new FileChooser();
+		exerciseChoosePopup.setTitle("Choose Exercise");
+		exerciseChoosePopup.getExtensionFilters().add( new FileChooser.ExtensionFilter("xml files", "*.xml"));
+		File exerciseFile = exerciseChoosePopup.showOpenDialog(primaryStage);
+		if(exerciseFile != null)
+			return Paths.get(exerciseFile.getAbsolutePath());
+		System.err.println("No file selected! getExercise() returns null");
+		return null;
 	}
 
-	public static Stage getPrimaryStage(){
-		return primaryStage;
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
