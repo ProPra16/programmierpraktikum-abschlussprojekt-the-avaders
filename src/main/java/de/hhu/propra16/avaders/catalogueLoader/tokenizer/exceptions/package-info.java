3a00cd20f8f46b3e666cfb36997fdfdaed95b7e5
Nikita Hasert <nikitahasert@gmx.de>
2016-07-14 07:21:56 +0200

    Shorten package info

--- src/main/java/de/hhu/propra16/avaders/catalogueLoader/tokenizer/exceptions/package-info.java
+++ src/main/java/de/hhu/propra16/avaders/catalogueLoader/tokenizer/exceptions/package-info.java
@@ -1,5 +1,5 @@
 /**
- * This package holds the exceptions that can be thrown by the {@link de.hhu.propra16.avaders.catalogueLoader.tokenizer.ExerciseTokenizer}
+ * Holds the exceptions that can be thrown by the {@link de.hhu.propra16.avaders.catalogueLoader.tokenizer.ExerciseTokenizer}
  * if an unexpected situation arises
  */
 package de.hhu.propra16.avaders.catalogueLoader.tokenizer.exceptions;
